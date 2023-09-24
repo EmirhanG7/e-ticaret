a46ca0caef282795a7367cbdac55bb72ad18da83
Şahan ÖZTEKİN <sahan.oztekin2@gmail.com>
2023-09-24 17:36:51 +0300

    urunler htmlden kaldırıldı jsden eklendi

--- javascript.js
+++ javascript.js
@@ -1,50 +1,69 @@
 const products = [
-  { id: 1, name: "Pantolon", price: 30 },
-  { id: 2, name: "Kazak", price: 40 },
-  { id: 3, name: "Tişört", price: 20 },
+  { id: 1, name: "Pantolon", price: 30, category: "Giyim", stock: 10 },
+  { id: 2, name: "Kazak", price: 40, category: "Giyim", stock: 15 },
+  { id: 3, name: "Tişört", price: 20, category: "Giyim", stock: 20 },
 ];
 
 let cart = [];
 let totalPrice = 0;
 
-// sepete eklemek için idlerine göre tıklama çağırma kısmı
-document.querySelector("#addToCart1").addEventListener("click", () => {
-  addToCart(1);
-});
+function renderProducts() {
+  const productContainer = document.querySelector("#productContainer");
 
-document.querySelector("#addToCart2").addEventListener("click", () => {
-  addToCart(2);
-});
+  for (const product of products) {
+    const productHtml = `
+          <div class="col-md-4">
+              <div class="card">
+                  <div class="card-body">
+                      <h5 class="card-title">${product.name}</h5>
+                      <p class="card-text">Fiyat: $${product.price}</p>
+                      <p class="card-text">Kategori: ${product.category}</p>
+                      <p class="card-text">Stok: <span id="stock${product.id}">${product.stock}</span></p>
+                      <button class="btn btn-primary addToCart" data-product-id="${product.id}">
+                          Sepete Ekle
+                      </button>
+                  </div>
+              </div>
+          </div>
+      `;
 
-document.querySelector("#addToCart3").addEventListener("click", () => {
-  addToCart(3);
-});
+    productContainer.innerHTML += productHtml;
+  }
 
-// ürün ekleme
-function addToCart(productId) {
-  // ürünü bulması
-  const product = products.find((item) => item.id === productId);
+  //butonların tıklama eylemi için sepet işlevi gerçekleştirir.
+  const addToCartButtons = document.querySelectorAll(".addToCart");
 
-  if (product) {
-    // ürünün stokları
-    const stockElement = document.querySelector(`#stock${productId}`);
-    const stock = parseInt(stockElement.textContent);
+  for (const button of addToCartButtons) {
+    button.addEventListener("click", (event) => {
+      const productId = event.target.dataset.productId;
+      // ürünü bulması
+      const product = products.find((item) => item.id === Number(productId));
 
-    if (stock > 0) {
-      // sepete pushla eklemesi için
-      cart.push(product);
-      updateCartBasket();
+      if (product) {
+        addToCart(product);
+      }
+    });
+  }
+}
 
-      // stoktan düşürmek içib
-      stockElement.textContent = stock - 1;
+// ürün  ekleme sepete.
+function addToCart(product) {
+  // ürünün stokları
+  const stockElement = document.querySelector(`#stock${product.id}`);
 
-      // sepette fiatı güncel tutmak için
-      totalPrice += product.price;
-      updateTotalPrice();
-    } else {
-      // olurda stok kalmazsa diye
-      alert("Stokta yeterli ürün yok.");
-    }
+  if (product.stock > 0) {
+    // sepete pushla eklemesi için
+    cart.push(product);
+    updateCartBasket();
+    // stoktan düşürmek için -1 ile -- de aynıymış..
+    product.stock--;
+    totalPrice += product.price;
+    updateTotalOPrice();
+    stockElement.textContent = product.stock;
+    // chatgptden yardım aldım.. yeni urun ekleyince stock dusmuyordu..
+  } else {
+    // olurda stok kalmazsa diye
+    alert("Stokta yeterli ürün yok.");
   }
 }
 
@@ -53,25 +72,23 @@ function updateCartBasket() {
   const cartList = document.querySelector("#cart");
   cartList.innerHTML = "";
 
-  let cartHTML = "";
-
   for (const product of cart) {
-    cartHTML += `<li>${product.name}</li>`;
+    cartList.innerHTML += `<li>${product.name}</li>`;
   }
-
-  cartList.innerHTML = cartHTML;
 }
 
-// map kullanarakta yapılırmış fakat çok zorlandım o yüzden çokta map üzerinde duramadım.
-// function updateCartBasket() {
-//     const cartList = document.querySelector("#cart");
-//     cartList.innerHTML = cart
-//       .map((product) => `<li>${product.name}</li>`)
-//       .join("");
-//   }
-
 // toplam fiyatımız
-function updateTotalPrice() {
+function updateTotalOPrice() {
   const totalPriceElement = document.querySelector("#totalPrice");
   totalPriceElement.textContent = totalPrice;
 }
+
+products.push({
+  id: 4,
+  name: "Yeni Ürün",
+  price: 50,
+  category: "Teknoloji",
+  stock: 5,
+});
+
+renderProducts();
